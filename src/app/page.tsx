e17e16bf978bc352c0d35028893110e485486c7c
Pablo Rumualdo <pablorumualdo2305@gmail.com>
2023-10-14 19:59:17 -0300

    Avances de la landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,37 @@
 import "@/app/global.css"
-import Link from 'next/link'
 import Image from "next/image"
+import Navbar from "@/components/Navbar"
 
 const Page = () => {
     return (
-        <div>
-            <nav className='flex justify-between'>
-                <h1 className="text-4xl font-semibold m-4">Crombie gastos</h1>
-                <div className='flex gap-x-4 m-4'>
-                    <Link href="/sign-in">Sign in</Link>
-                    <Link href="/sign-up">Sign up</Link>
-                </div>
-            </nav>
-            <div className="w-40 h-40">
-                
-            </div>
+        <div className="bg-slate-500">
+            <Navbar/>
+            <main className="w-full h-[calc(100vh-10rem)] absolute flex justify-between items-center">
+                <section className="w-1/2 mx-10">
+                    <h2 className="text-[58px] font-bold font-principal">
+                        A special credit card made for Developers.</h2>
+                    <p className="font-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Vehicula massa in enim luctus. Rutrum arcu.</p>
+                    <div className="border-2 rounded-lg w-1/2 py-2 flex justify-between mt-10">
+                        <input type="email" placeholder="Enter e-mail adress"/>
+                        <button className="rounded-lg bg-slate-950 text-white font-semibold py-3 px-5 mr-1">Get Free Card</button>
+                    </div>
+                    <div className="flex mt-10 items-center">
+                        <p className="border-r-4">
+                            <span className="font-semibold text-[42px]">2943</span>
+                            Cards Delivered
+                        </p>
+                        <p>
+                            <span className="font-semibold text-[42px]">1M+</span>
+                            Happy Users
+                        </p>
+                    </div>
+                </section>
+                <section className="w-1/2 h-1/2 relative">
+                    <Image src={"/card2.svg"} alt="" fill />
+                    <Image src={"/card1.svg"} alt="" fill/>
+                </section>
+            </main>
         </div>
     )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className='flex justify-between'>
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold m-4 font-principal tracking-wider">CrombieBudget</h1>
+                </Link>
+                <div className='flex gap-x-4 m-4 items-center '>
+                    <div className='text'>
+                        <Link href="/sign-in">Sign in</Link>
+                    </div>
+                    <div className='rounded-lg bg-slate-950 text-white font-semibold py-3 px-5 mr-1'>
+                        <Link href="/sign-up">Sign up</Link>
+                    </div>
+                </div>
+            </nav></div>
+    )
+}
+
+export default Navbar
